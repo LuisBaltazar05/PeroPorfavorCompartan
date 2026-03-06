cfc380556f6768e6ac379684cd2d902abdaaedaf
Luis Angel Baltazar Gonzalez <luisangelbaltazarglz@outlook.com>
2026-03-06 13:42:46 -0600

    "Bug de botones corregidos arreglado"

--- js/budget.js
+++ js/budget.js
@@ -4,7 +4,10 @@ document.querySelectorAll(".budget-btn").forEach(function(btn){
 
   btn.addEventListener("click",function(){
 
-    document.querySelectorAll(".budget-btn").forEach(b=>b.classList.remove("btn-accent"))
+    document.querySelectorAll(".budget-btn").forEach(b=>{
+      b.classList.remove("btn-accent")
+      b.classList.add("btn-outline-light")
+    })
 
     btn.classList.add("btn-accent")
 
